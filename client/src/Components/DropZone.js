7c0276998d59c690e164e4e5a65917c63b91c21e
harish-r <harish23r@gmail.com>
2018-11-11 19:33:01 -0700

    drag and drop, errors remain

--- client/src/Components/DropZone.js
+++ client/src/Components/DropZone.js
@@ -23,7 +23,6 @@ class DropZone extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            dataPointList: [], // List of objects having details about circles
             nodes: [
             ]
         };
@@ -77,6 +76,7 @@ class DropZone extends React.Component {
     /* On dropping a point, 
      */
     addDataPoint(event) {
+        let that = this.props;
         event.preventDefault();
         let self = this
         console.log("Dropped ... Adding point ...");
@@ -88,7 +88,7 @@ class DropZone extends React.Component {
         console.log(dataPoint);
         /* Add data point to the nodes array in the state of this component*/ 
         /* TODO: Changing state manually, change state using setState() method*/
-        self.state.node.push(dataPoint);
+        self.setState({nodes: self.state.nodes.push(dataPoint)});
 
         // Update the force layout and re-render the svg
         self.simulation = forceSimulation(self.state.nodes)
@@ -105,11 +105,15 @@ class DropZone extends React.Component {
                 circles.enter()
                     .append('circle')
                     .attr('r', function (d) {
-                        return 10
+                        return 7;
                     })
-                    .on('dblclick', function(d, i) {
-                        console.log("Double click...");
-                        event.dat
+                    .on("dblclick", function (d, i) {
+                        // event.preventDefault();
+                        console.log("Double click on data point");
+                        console.log("Datapoint: ");
+                        console.log(d);
+                        
+                        self.removeDataPoint(dataPoint);
                     })
                     .merge(circles)
                     .attr('cx', function (d) {
@@ -122,19 +126,59 @@ class DropZone extends React.Component {
                 circles.exit()
                     .remove();
             });
-        /* 
-         * TODO: Invoke callback function, passing the dataPoint as data 
-         * 
-        */
+        /* Invoke callback function, passing the dataPoint as data */
+        let currDataPoints = self.state.nodes;
+        let success = that.removeDataPointFromScatter(currDataPoints, that.position);
+        if (success) {
+            console.log("Success");
+        }
+        else {
+            console.log("Failure");
+        }
     }
 
-    removeDataPoint(event) {
-        event.preventDefault();
+    removeDataPoint(dataPoint) {
+        let self = this
         console.log("Double Click ...");
+        let that = this.props;
         /* TODO: uniquely identify the data point */
-        /* Remove data point from nodes in state */
+        /* TODO: Remove data point from nodes in state */
         /* Update the force layout and re-render */
+        self.simulation = forceSimulation(self.state.nodes)
+            .force('charge', forceManyBody().strength(5))
+            .force('center', forceCenter(self.width / 2, self.height / 2))
+            .force('collision', forceCollide().radius(function (d) {
+                return 7;
+            }))
+            .on('tick', function () {
+                var circles = select(".svg")
+                    .selectAll('circle')
+                    .data(self.state.nodes);
+
+                circles.enter()
+                    .append('circle')
+                    .attr('r', function (d) {
+                        return 7;
+                    })
+                    .merge(circles)
+                    .attr('cx', function (d) {
+                        return d.x
+                    })
+                    .attr('cy', function (d) {
+                        return d.y
+                    });
+
+                circles.exit()
+                    .remove();
+            });
         /* Invoke callback to add dataPoint to the scatterplot */
+        let currDataPoints = this.state.nodes;
+        let success = that.addDataPointFromScatter(currDataPoints, that.position);
+        if (success) {
+            console.log("Success");
+        } else {
+            console.log("Failure");
+        }
     }
 
     dragOverHandler(event) {
@@ -150,7 +194,6 @@ class DropZone extends React.Component {
                 style={dropZoneDiv} 
                 onDragOver={this.dragOverHandler}
                 onDragLeave={this.dragLeaveHandler}
-                onDoubleClick={this.removeDataPoint} 
                 onDrop={this.addDataPoint} >
                 <svg width={this.width} ref="svg" height={this.height} className="svg">
                 </svg>
