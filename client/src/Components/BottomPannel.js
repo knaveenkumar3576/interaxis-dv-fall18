8b219f8d1e8f7be319e643c11825e43b67ee07b1
Satheesh Kurunthiah <satheeshkurunthiah@Satheeshs-MacBook-Pro.local>
2018-11-12 18:11:04 -0700

    fixed bottom pannel issue

--- client/src/Components/BottomPannel.js
+++ client/src/Components/BottomPannel.js
@@ -46,11 +46,11 @@ class BottomPannel extends React.Component {
             <GridLayout className="layout" layout={columnLayout} cols={12}
                         rowHeight={this.state.height} width={this.state.width}>
                 <div ref={'xMinDropZone'} key="a">
-                    <DropZone position={"xMin"} height={this.state.yMinDropZoneHeight}
-                        width={this.state.yMinDropZoneWidth}
-                        dataset = {this.props.dataset}
-                        addDataPointCallback={this.props.removeDataPointFromScatterCallback}
-                        removeDataPointCallback={this.props.addDataPointToScatterCallback} />
+                    <DropZone position={"xMin"} height={this.state.xMinDropZoneHeight}
+                              width={this.state.xMinDropZoneWidth}
+                              dataset={this.props.dataset}
+                              addDataPointCallback={this.props.removeDataPointFromScatterCallback}
+                              removeDataPointCallback={this.props.addDataPointToScatterCallback}/>
                 </div>
                 <div ref={'rightBar'} key="b" id={'rightBarChart'}>
                     {this.state.rightBarHeight > 0 && this.state.rightBarWidth > 0 ?
@@ -58,12 +58,12 @@ class BottomPannel extends React.Component {
                                   barWidth={10} id={'rightBarChart'}/> : null}
                 </div>
                 <div ref={'xMaxDropZone'} key="c">
-                    <DropZone position={"xMax"} 
-                        height={this.state.yMinDropZoneHeight}
-                        width={this.state.yMinDropZoneWidth}
-                        dataset = {this.props.dataset}
-                        addDataPointCallback={this.props.removeDataPointFromScatterCallback}
-                        removeDataPointCallback={this.props.addDataPointToScatterCallback} />
+                    <DropZone position={"xMax"}
+                              height={this.state.xMinDropZoneHeight}
+                              width={this.state.xMinDropZoneWidth}
+                              dataset={this.props.dataset}
+                              addDataPointCallback={this.props.removeDataPointFromScatterCallback}
+                              removeDataPointCallback={this.props.addDataPointToScatterCallback}/>
                 </div>
             </GridLayout>
         );
