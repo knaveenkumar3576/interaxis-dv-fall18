b6406cf1c426af0527e6f9672cf2aa5288a25cda
Naveen Kumar K <knaveenkumar3576@gmail.com>
2018-11-28 19:57:12 -0700

    search and compare done

--- client/src/containers/Compare.js
+++ client/src/containers/Compare.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import Select from 'react-select';
+import * as Papa from 'papaparse';
+import DataPointDetail from '../Components/DataPointDetail';
+import MultiScatterPlot from '../Components/MultiScatterPlot';
+
+
+const KEYS_TO_BE_USED = {
+    sample: ['x', 'y', 'z'],
+    census: ['TotalPop', 'Men', 'Women', 'Hispanic', 'White', 'Black', 'Native', 'Asian', 'Pacific', 'Citizen', 'Income', 'IncomeErr', 'IncomePerCap', 'IncomePerCapErr', 'Poverty', 'ChildPoverty', 'Professional', 'Service', 'Office', 'Construction', 'Production', 'Drive', 'Carpool', 'Transit', 'Walk', 'OtherTransp', 'WorkAtHome', 'MeanCommute', 'Employed', 'PrivateWork', 'PublicWork', 'SelfEmployed', 'FamilyWork', 'Unemployment'],
+    car: ['Sedan', 'Sports Car', 'SUV', 'Wagon', 'Minivan', 'Pickup', 'AWD', 'RWD', 'Retail Price', 'Dealer Cost', 'Engine Size', 'Cyl', 'HP', 'City MPG', 'Hwy MPG', 'Weight', 'Wheel Base', 'Len', 'Width'],
+    football: ['Age', 'Overall', 'Potential', 'Special', 'Acceleration', 'Aggression', 'Agility', 'Balance', 'Ball control', 'Composure', 'Crossing', 'Curve', 'Dribbling', 'Finishing', 'Free kick accuracy', 'GK diving', 'GK handling', 'GK kicking', 'GK positioning', 'GK reflexes', 'Heading accuracy', 'Interceptions', 'Jumping', 'Long passing', 'Long shots', 'Marking', 'Penalties', 'Positioning', 'Reactions', 'Short passing', 'Shot power', 'Sliding tackle', 'Sprint speed', 'Stamina', 'Standing tackle', 'Strength', 'Vision', 'Volleys', 'CAM', 'CB', 'CDM', 'CF', 'CM', 'LAM', 'LB', 'LCB', 'LCM', 'LDM', 'LF', 'LM', 'LS', 'LW', 'LWB', 'RAM', 'RB', 'RCB', 'RCM', 'RDM', 'RF', 'RM', 'RS', 'RW', 'RWB', 'ST']
+};
+
 
 class Compare extends React.Component {
     constructor(props) {
@@ -7,7 +19,17 @@ class Compare extends React.Component {
         this.state = {
             versions: props.versions,
             multiValue: [],
-            filterOptions: []
+            filterOptions: [],
+            dataPoints1: [],
+            dataPoints2: [],
+            originalDataPoints1: [],
+            originalDataPoints2: [],
+            selectedLabels1 : {},
+            selectedLabels2: {},
+            currDataPoint: null,
+            scatterPlotWidth: 800,
+            scatterPlotHeight: 600,
+
         };
         this.handleMultiChange = this.handleMultiChange.bind(this);
     }
@@ -17,23 +39,183 @@ class Compare extends React.Component {
             multiValue: option
         });
         if (option.length > 1) {
-            this.props.onCompareChange(option.map(attr => {
+            this.onCompareChange(option.map(attr => {
                 return attr.label;
             }));
         }
     }
 
+    scatterOnMouseOverCallback(i) {
+        this.setState({currDataPoint: this.state.originalDataPoints1[i]});
+    }
+
+    calculateCustomValues = (versioninfo, selectedIndex) => {
+
+        let dataPointsxMin =  versioninfo.xMin || [];
+        let dataPointsxMax =  versioninfo.xMax || [];
+        let dataPointsyMin =  versioninfo.yMin || [];
+        let dataPointsyMax =  versioninfo.yMax || [];
+
+        let noOfDataPointsXMin = dataPointsxMin.length;
+        let noOfDataPointsXMax = dataPointsxMax.length;
+
+        let noOfDataPointsYMin = dataPointsyMin.length;
+        let noOfDataPointsYMax = dataPointsyMax.length;
+
+        let customX = {};
+        let customY = {};
+
+        let storedKeyLabel = "selectedLabels" + selectedIndex;
+        let storedKeyDataPoints = "dataPoints" + selectedIndex;
+
+        if (noOfDataPointsXMin > 0 && noOfDataPointsXMax > 0) {
+            KEYS_TO_BE_USED[versioninfo.dataset].forEach(key => {
+                let minWeights = 0;
+
+                dataPointsxMin.forEach(d => {
+                    minWeights += d[key];
+                });
+                let avgMinValue = minWeights / noOfDataPointsXMin;
+
+                let maxWeights = 0;
+                dataPointsxMax.forEach(d => {
+                    maxWeights += d[key];
+                });
+                let avgMaxValue = maxWeights / noOfDataPointsXMax;
+                customX[key] = avgMaxValue - avgMinValue;
+            });
+
+            this.state[storedKeyDataPoints].forEach(d => {
+                let x = 0;
+                KEYS_TO_BE_USED[versioninfo.dataset].forEach(key => {
+                    x += d[key] * customX[key];
+                });
+                d["customX"] = x;
+            });
+        }
+
+        if (noOfDataPointsYMin > 0 && noOfDataPointsYMax > 0) {
+                KEYS_TO_BE_USED[this.state.dataset].forEach(key => {
+                let minWeights = 0;
+
+                dataPointsyMin.forEach(d => {
+                    minWeights += d[key];
+                });
+                let avgMinValue = minWeights / noOfDataPointsYMin;
+
+                let maxWeights = 0;
+                dataPointsyMax.forEach(d => {
+                    maxWeights += d[key];
+                });
+                let avgMaxValue = maxWeights / noOfDataPointsYMax;
+                customY[key] = avgMaxValue - avgMinValue;
+            });
+
+            this.state[storedKeyDataPoints].forEach(d => {
+                let y = 0;
+                KEYS_TO_BE_USED[versioninfo.dataset].forEach(key => {
+                    y += d[key] * customY[key];
+                });
+                d["customY"] = y;
+            });
+        }
+
+        let labels = {
+            x : versioninfo.xAttribute,
+            y : versioninfo.yAttribute
+        };
+
+        this.setState({
+            [storedKeyLabel]: labels
+        });
+
+    };
+
+    processAndNormalizeData = (result, versioninfo, selectedIndex) => {
+
+        let keysToNormalize = KEYS_TO_BE_USED[versioninfo.dataset];
+        let resultdataPoints = result.data;
+        keysToNormalize.forEach(key => {
+            let max = Math.max.apply(null, resultdataPoints.map(d => d[key]));
+            let min = Math.min.apply(null, resultdataPoints.map(d => d[key]));
+            resultdataPoints.forEach(point => {
+                point[key] = (point[key] - min) / (max - min);
+            })
+        });
+
+        let storedKey = "dataPoints" + selectedIndex;
+        this.setState({ [storedKey]: resultdataPoints });
+        this.calculateCustomValues(versioninfo, selectedIndex);
+    };
+
+    saveOriginalData = (result, selectedIndex) => {
+        let storedKey = "originalDataPoints" + selectedIndex;
+        this.setState({ [storedKey]: JSON.parse(JSON.stringify(result.data)) });
+    };
+
+    changeData = (versioninfo, selectedIndex) => {
+
+        console.log(versioninfo);
+        let csvFilePath = require("../data/" + versioninfo.dataset + ".csv");
+
+        Papa.parse(csvFilePath, {
+            header: true,
+            download: true,
+            skipEmptyLines: true,
+            complete: (result) => {
+                this.saveOriginalData(result, selectedIndex);
+                this.processAndNormalizeData(result, versioninfo, selectedIndex);
+            }
+        });
+    };
+
+
+    onCompareChange(options) {
+        let compareData = [], that = this;
+
+        options.forEach(function (option) {
+            compareData.push(that.props.savedInfo.filter(attr => {
+                return attr.version === option
+            }));
+        });
+        this.changeData(compareData[0][0], 1);
+        this.changeData(compareData[1][0], 2);
+    }
+
+
     render() {
+
         let filterOptions = this.state.versions.map((attr, index) => {
             return {value: index, label: attr.name}
         });
+
+
         return (
             <div align="center" style={{width: '50%', margin: 'auto'}}>
                 {filterOptions.length > 0 ?
-                    <Select className={'compare'} placeholder="Select at least 2 versions to compare"
+                    <div>
+                        <Select className={'compare'} placeholder="Select at least 2 versions to compare"
                             value={this.state.multiValue} options={filterOptions}
-                            onChange={this.handleMultiChange} isMulti/> : null}
-            </div>
+                            onChange={this.handleMultiChange} isMulti
+                        />
+                        <div ref={'comaparescatterPlot'} id={'comparescatterPlotId'} style={{height: '70%'}}>
+
+                            <MultiScatterPlot 
+                                id={'comparescatterPlotId'}
+                                dataPoints1={this.state.dataPoints1}
+                                labels1={this.state.selectedLabels1}
+                                dataPoints2={this.state.dataPoints2}
+                                labels2={this.state.selectedLabels2}
+                                width={this.state.scatterPlotWidth}
+                                height={this.state.scatterPlotHeight}
+                                detailViewCallback={this.scatterOnMouseOverCallback.bind(this)}
+                            />
+                        </div>
+                        <DataPointDetail dataPointDetails={this.state.currDataPoint}/>
+                    </div>
+                    : null
+                }
+           </div>
         );
     }
 }
